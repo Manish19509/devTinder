ac6c8409909804125192ef696b36aed7d7c95f06
Manish <manishkeshri19509@gmail.com>
2025-07-20 03:45:40 +0530

    commit

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+//user schema
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  emailId: {
+    type: String,
+  },
+  password: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+  gender: {
+    type: String,
+  },
+});
+
+//mongoose model
+module.exports = mongoose.model("User", userSchema); // User will always start with capital, since we will create new instances of it as we do with class
