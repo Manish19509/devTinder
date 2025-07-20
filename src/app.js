d2eaa8d6410e2f6915b76a17babb65097878f2eb
Manish <manishkeshri19509@gmail.com>
2025-07-20 16:45:20 +0530

    dynamic data in signup API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
 const app = express();
 const User = require("./models/user"); //User model
 
+app.use(express.json()); //WE will have to use middleware bcz dynamic data is comming in json format and it will convert json in js object and we can use that data
+
 app.post("/signup", async (req, res) => {
+  //for dynamic data
+  const user = new User(req.body);
+
+  //this is a hardcoded data - not for dynamic
+
   // const useObj = {
   //   firstName: "Manish",
   //   lastName: "Keshri",
@@ -14,14 +20,15 @@ app.post("/signup", async (req, res) => {
   // //creating a new instance of the User Model
   // const user = new User(userObj);
 
-  //or we can write
-  const user = new User({
-    //we can also set id but it will be better to keep it done by mongoDB
-    firstName: "Rahul",
-    lastName: "Keshri",
-    emailId: "Rahul@gmail.com",
-    Password: "Rahul@123",
-  });
+  //or we can write below code
+
+  // const user = new User({
+  //   //we can also set id but it will be better to keep it done by mongoDB
+  //   firstName: "Rahul",
+  //   lastName: "Keshri",
+  //   emailId: "Rahul@gmail.com",
+  //   Password: "Rahul@123",
+  // });
 
   try {
     await user.save(); //this function will return promise , most of the mongoose func return promise so use async and awi
